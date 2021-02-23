201f41f43ff18fea9f1d68078efccf48f75fd84e
Werner Luiz Gottschalt <werner@lumiun.com>
2021-02-23 17:32:42 -0300

    criando o encapsulamento da classe

--- src/Conta.php
+++ src/Conta.php
@@ -1,7 +1,39 @@
 <?php
 
     class Conta {
-        public string $cpfTitular;
-        public string $nomeTitular;
-        public float $saldo;
+        private string $cpfTitular;
+        private string $nomeTitular;
+        private float $saldo;
+
+
+
+        public function getCpfTitular() {
+            return $this->cpfTitular;
+        }
+
+        public function getNomeTitular() {
+            return $this->nomeTitular;
+        }
+
+        public function setNomeTitular() {
+            $this->nomeTitular = $nomeTitular;
+        }
+
+        public function getSaldo() {
+            return $this->saldo;
+        }
+
+        public function setSaldo() {
+            $this->saldo = $saldo;
+        }
+
+
+        public function sacar(float $valorASacar) {
+            if ($valorASacar > $this->saldo) {
+                echo "Saldo insuficiente";
+            } else {
+                $this->saldo -= $valorASacar;
+                echo "Saque realizado com sucesso";
+            }
+        }
     }
