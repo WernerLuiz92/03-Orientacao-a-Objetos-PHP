9d19a4c2ecc756229fe3145584e6dcbb3693ede7
Werner Luiz Gottschalt <fera-luiz-gottschalt@hotmail.com>
2021-02-23 22:08:40 -0300

    Implementando o metodo depositar

--- src/Conta.php
+++ src/Conta.php
@@ -1,42 +1,60 @@
 <?php
 
-    class Conta {
+    class Conta 
+    {
         private string $cpfTitular;
         private string $nomeTitular;
         private float $saldo;
 
 
-        public function getCpfTitular() {
+        public function getCpfTitular() 
+        {
             return $this->cpfTitular;
         }
 
-        public function setCpfTitular($cpfTitular) {
+        public function setCpfTitular($cpfTitular) 
+        {
             $this->cpfTitular = $cpfTitular;
         }
 
-        public function getNomeTitular() {
+        public function getNomeTitular() 
+        {
             return $this->nomeTitular;
         }
 
-        public function setNomeTitular($nomeTitular) {
+        public function setNomeTitular($nomeTitular) 
+        {
             $this->nomeTitular = $nomeTitular;
         }
 
-        public function getSaldo() {
+        public function getSaldo() 
+        {
             return $this->saldo;
         }
 
-        public function setSaldo($saldo) {
+        public function setSaldo($saldo) 
+        {
             $this->saldo = $saldo;
         }
 
 
-        public function sacar(float $valorASacar) {
+        public function sacar(float $valorASacar): void
+        {
             if ($valorASacar > $this->saldo) {
                 echo "Saldo insuficiente";
             } else {
                 $this->saldo -= $valorASacar;
                 echo "Saque realizado com sucesso";
+            }
+        }
+
+        public function depositar(float $valorADepositar): void
+        {
+            if ($valorADepositar < 0) {
+                echo "O valor a ser depositado deve ser maior do que zero!";
+            } else {
+                $this->saldo += $valorADepositar;
+                echo "Deposito efetuado com sucesso";
             }
         }
     }
