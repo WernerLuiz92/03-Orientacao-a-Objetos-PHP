5e2e144c056fc513b634a329759e96ac2e9d30fd
Werner Luiz Gottschalt <fera-luiz-gottschalt@hotmail.com>
2021-02-23 22:48:27 -0300

    Tipando os paramentros dos metodos

--- src/Conta.php
+++ src/Conta.php
@@ -7,32 +7,32 @@
         private float $saldo = 0;
 
 
-        public function getCpfTitular() 
+        public function getCpfTitular(): string  
         {
             return $this->cpfTitular;
         }
 
-        public function setCpfTitular($cpfTitular) 
+        public function setCpfTitular(string $cpfTitular) 
         {
             $this->cpfTitular = $cpfTitular;
         }
 
-        public function getNomeTitular() 
+        public function getNomeTitular(): string 
         {
             return $this->nomeTitular;
         }
 
-        public function setNomeTitular($nomeTitular) 
+        public function setNomeTitular(string $nomeTitular) 
         {
             $this->nomeTitular = $nomeTitular;
         }
 
-        public function getSaldo() 
+        public function getSaldo(): float 
         {
             return $this->saldo;
         }
 
-        public function setSaldo($saldo) 
+        public function setSaldo(float $saldo) 
         {
             $this->saldo = $saldo;
         }
